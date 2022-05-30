eebcecf3d3d3035c1c7c3a3b8f4fc116e0fa344f
Parallels <jcyesc@gmail.com>
2022-05-30 15:59:10 -0700

    How to use red black trees in Linux
    It presents an examples that shows how to use rb_root and rb_node.
    
    The module was tested using
    
    sudo insmod users_rbtree.ko
    dmesg
    sudo rmmod users_rbtree
    
    The output looks like:
    
    [ 6376.303682] Initializing rbtree module
    [ 6376.303695] Inserting users
    [ 6376.303702] Traversing in postorder
    [ 6376.303713] user [id: 21, name: alpha]
    [ 6376.303720] user [id: 22, name: travis]
    [ 6376.303726] user [id: 24, name: hacker]
    [ 6376.303733] user [id: 23, name: super]
    [ 6376.303739] user [id: 26, name: root]
    [ 6376.303745] user [id: 25, name: king]
    [ 6376.303752] Finding users
    [ 6376.303758] user with id=20 was not found
    [ 6376.303764] user [id: 21, name: alpha]
    [ 6376.303770] user [id: 22, name: travis]
    [ 6376.303776] user [id: 23, name: super]
    [ 6376.303783] user [id: 24, name: hacker]
    [ 6376.303789] user [id: 25, name: king]
    [ 6376.303795] user [id: 26, name: root]
    [ 6376.303802] user with id=27 was not found
    [ 6376.303808] user with id=28 was not found
    [ 6376.303814] user with id=29 was not found
    [ 6376.303814] Exiting rbtree module

---
+++ labs/kernel-intro/ch5-kernel-programming-preview/mod-rbtree/users_rbtree.c
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Module that shows how to use the red black tree library in Linux.
+ *
+ * - define root tree
+ * - insert nodes
+ * - find nodes
+ * - traverse tree
+ */
+
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/rbtree.h>
+#include <linux/types.h>
+
+#define USER_NAME_SIZE 40
+struct usertype {
+	unsigned int id;
+	char name[USER_NAME_SIZE];
+	struct rb_node user_rb;
+};
+
+#define NUM_USERS 6
+static struct usertype users[NUM_USERS] = {
+		{.id = 26, .name = "root",   .user_rb = {}},
+		{.id = 25, .name = "king",   .user_rb = {}},
+		{.id = 24, .name = "hacker", .user_rb = {}},
+		{.id = 23, .name = "super",  .user_rb = {}},
+		{.id = 22, .name = "travis", .user_rb = {}},
+		{.id = 21, .name = "alpha",  .user_rb = {}},
+};
+
+static struct rb_root root = RB_ROOT;
+
+static struct usertype *rb_search_user(unsigned int id)
+{
+	struct rb_node *n = root.rb_node;
+
+	while (n) {
+		struct usertype *user = rb_entry(n, struct usertype, user_rb);
+
+		if (id < user->id)
+			n = n->rb_left;
+		else if (id > user->id)
+			n = n->rb_right;
+		else
+			return user;
+	}
+
+	return NULL;
+}
+
+/*
+ * Inserts a node in the red black tree.
+ *
+ * Returns a pointer to struct user if it is already in the tree, otherwise it
+ * returns NULL.
+ */
+static struct usertype *rb_insert_user(struct usertype *newuser)
+{
+	struct rb_node **p = &root.rb_node;
+	struct rb_node *parent = NULL;
+	struct usertype *user;
+	unsigned int id = newuser->id;
+
+	while (*p) {
+		parent = *p;
+		user = rb_entry(parent, struct usertype, user_rb);
+
+		if (id < user->id)
+			p = &(*p)->rb_left;
+		else if (id > user->id)
+			p = &(*p)->rb_right;
+		else
+			return user;
+	}
+
+	rb_link_node(&newuser->user_rb, parent, p);
+	rb_insert_color(&newuser->user_rb, &root);
+
+	return NULL;
+}
+
+static inline void traverse_rb(void)
+{
+	struct usertype *user, *next;
+
+	pr_info("Traversing in postorder");
+
+	rbtree_postorder_for_each_entry_safe(user, next, &root, user_rb) {
+		pr_info("user [id: %d, name: %s]", user->id, user->name);
+	}
+}
+
+static int __init users_rbtree_init(void)
+{
+	unsigned int i;
+	struct usertype *user;
+	unsigned int id;
+
+	pr_info("Initializing rbtree module");
+
+	pr_info("Inserting users\n");
+	for (i = 0; i < NUM_USERS; i++)
+		rb_insert_user(&users[i]);
+
+	traverse_rb();
+
+	pr_info("Finding users\n");
+	for (i = 0; i < NUM_USERS + 5; i++) {
+		id = i + 20;
+		user = rb_search_user(id);
+		if (user)
+			pr_info("user [id: %d, name: %s]", user->id, user->name);
+		else
+			pr_info("user with id=%d was not found", id);
+	}
+
+	return 0;
+}
+
+static void __exit users_rbtree_exit(void)
+{
+	pr_info("Exiting rbtree module");
+}
+
+module_init(users_rbtree_init);
+module_exit(users_rbtree_exit);
+
+MODULE_AUTHOR("Juan Yescas");
+MODULE_DESCRIPTION("Red black trees library usage");
+
