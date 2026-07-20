6d2363967ef338c411a09de6df469cd570a44a36
jcyesc@gmail.com <jcyesc@gmail.com>
2026-07-20 11:22:54 -0700

    ch5: Trying to xa_erase() an item that does not exist
    When xa_erase() is called with an index that it doesn't exist in
    the xarray, it returns null.

--- labs/kernel-intro/ch5-kernel-programming-preview/mod-xarray/xarray_basic.c
+++ labs/kernel-intro/ch5-kernel-programming-preview/mod-xarray/xarray_basic.c
@@ -8,8 +8,8 @@
  * - Find data by id in the xarray
  * - Free xarray
  *
- * Note: idr is the map implementation in the linux kernel that only supports
- *       numbers.
+ * Note: idr is the map implementation in the linux kernel that
+ *       only supports numbers.
  *
  * More information in:
  *
@@ -33,71 +33,75 @@ struct user {
 };
 
 static struct user users[NUM_USERS] = {
-	{ .id = 0, .name = "root:0" },
-	{ .id = 1, .name = "silver:1" },
-	{ .id = 2, .name = "diana:2" },
-	{ .id = 3, .name = "mega:3" },
-	{ .id = 4, .name = "user:4" },
+		{.id = 0, .name = "root:0"},
+        {.id = 1, .name = "silver:1"},
+		{.id = 2, .name = "diana:2"},
+        {.id = 3, .name = "mega:3"},
+		{.id = 4, .name = "user:4"},
 };
 
 static int __init xarray_mod_init(void)
 {
-    struct user *user;
-    unsigned long index;
-    int ret;
+	struct user *user;
+	unsigned long index;
+	int ret;
 
-    pr_info("Basic example of xarray\n");
+	pr_info("Basic example of xarray\n");
 
-    for (int i = 0; i < NUM_USERS; i++) {
-        ret = xa_insert(&user_xarray, i, &users[i], GFP_KERNEL);
-        if (ret)
-            pr_err("Failed to insert in xarray");
-    }
+	for (int i = 0; i < NUM_USERS; i++) {
+		ret = xa_insert(&user_xarray, i, &users[i], GFP_KERNEL);
+		if (ret)
+			pr_err("Failed to insert in xarray");
+	}
 
+	for (int i = NUM_USERS - 1; i >= 0; i--) {
+		user = xa_load(&user_xarray, i);
+		if (user)
+			pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
+	}
 
-    for (int i = NUM_USERS -1; i >=0; i--) {
-        user = xa_load(&user_xarray, i);
-        if (user)
-            pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
-    }
+	// Unknown index.
+	user = xa_load(&user_xarray, 45);
+	if (!user)
+		pr_info("xa_load() didn't find the id: %d", 45);
 
-    // Unknown index.
-    user = xa_load(&user_xarray, 45);
-    if (!user)
-        pr_info("xa_load() didn't find the id: %d", 45);
+	pr_info("Using xa_for_each() to iterate:\n");
+	// Note that index hasn't been initialized.
+	xa_for_each(&user_xarray, index, user) {
+		pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
+	}
 
-    pr_info("Using xa_for_each() to iterate:\n");
-    // Note that index hasn't been initialized.
-    xa_for_each(&user_xarray, index, user) {
-        pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
-    }
+	user = xa_erase(&user_xarray, 3);
+	if (user)
+		pr_info("Erased user with id: %d and name: %s)\n", user->id, user->name);
 
-    user = xa_erase(&user_xarray, 3);
-    if (user) {
-        pr_info("Erased user with id: %d and name: %s)\n", user->id, user->name);
-    }
+	pr_info("Trying to erase an entry that does not exist");
+	user = xa_erase(&user_xarray, 2253);
+	if (!user)
+		pr_info("Entry was NOT erased!");
 
-    for (int i = NUM_USERS -1; i >=0; i--) {
-        user = xa_load(&user_xarray, i);
-        if (user)
-            pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
-    }
+	for (int i = NUM_USERS - 1; i >= 0; i--) {
+		user = xa_load(&user_xarray, i);
+		if (user)
+			pr_info("xa_load id: %d, name: %s\n", user->id, user->name);
+	}
 
-    return 0;
+	return 0;
 }
 
 static void __exit xarray_mod_exit(void)
 {
-    struct user *user;
-    unsigned long index;
+	struct user *user;
+	unsigned long index;
 
-    xa_for_each(&user_xarray, index, user) {
-        pr_info("Destroying index: %lu, id: %d, name: %s\n", index, user->id, user->name);
-        xa_erase(&user_xarray, index);
-        //kfree(entry);
-    }
+	xa_for_each(&user_xarray, index, user) {
+		pr_info("Destroying index: %lu, id: %d, name: %s\n", index, user->id,
+		    user->name);
+		xa_erase(&user_xarray, index);
+		// kfree(entry);
+	}
 
-    xa_destroy(&user_xarray);
+	xa_destroy(&user_xarray);
 }
 
 module_init(xarray_mod_init);
@@ -106,4 +110,3 @@ module_exit(xarray_mod_exit);
 MODULE_AUTHOR("Juan Yescas");
 MODULE_DESCRIPTION("xarray basic example");
 MODULE_LICENSE("GPL");
-
